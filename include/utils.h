b16d62495d5b48e438580f9125858a99e70409b6
rsudo <badbad7351@gmail.com>
2022-01-26 19:49:45 +0900

    add utils.h

--- include/utils.h
+++ include/utils.h
@@ -29,4 +29,11 @@ char	*rec_gnl(ssize_t i, t_gnl *gnl, int fd);
 void	put_away_line(t_gnl *gnl);
 int		get_next_line(int fd, char **line);
 
+void	*xmalloc(size_t size);
+size_t	ft_strlen(const char *str);
+char	**ft_split(char *s, char c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strdup(const char *str);
+void	xput_error(char *str);
+
 #endif
