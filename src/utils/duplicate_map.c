b167b3682d7fc481f7efc61ce24fbf4855724442
rsudo <badbad7351@gmail.com>
2022-01-29 12:16:34 +0900

    adjast to norm

--- src/utils/duplicate_map.c
+++ src/utils/duplicate_map.c
@@ -12,19 +12,18 @@ static size_t	count_map_row(char **map)
 	return (i);
 }
 
-char **duplicate_map(char **map)
+char	**duplicate_map(char **map)
 {
 	char	**copy;
 	size_t	i;
 
 	i = 0;
-	copy = (char**)xmalloc((sizeof(char *)) * (count_map_row(map) + 1));
+	copy = (char **)xmalloc((sizeof(char *)) * (count_map_row(map) + 1));
 	while (map[i])
 	{
 		copy[i] = ft_strdup(map[i]);
 		i++;
 	}
 	copy[i] = NULL;
-	return copy;
+	return (copy);
 }
-
