3e20044ab12d363535616a00df0716e8c1747bd3
rsudo <badbad7351@gmail.com>
2022-02-03 22:41:36 +0900

    refactor test

--- test/test.sh
+++ test/test.sh
@@ -8,11 +8,11 @@ execute() {
 	(./cub3D $1 2>$2) & sleep ${ALIVE_TIME}
 
 	if [ -s $2 ]; then
-		echo -e "${GREEN}[[${2}]]${RESET}\nOK"
+		echo -e "${GREEN}[[ ${2} ]]${RESET}\nOK"
 	else
-		echo -e "${GREEN}[[${2}]]${RESET}\nNG"
+		echo -e "${GREEN}[[ ${2} ]]${RESET}\nNG"
 	fi
-	rm $2
+	rm -f $2
 }
 
 cubfiles=`find ./cubfiles -name "error*"`
@@ -20,6 +20,8 @@ for filepath in $cubfiles; do
 	NAME=`basename $filepath`
 	execute $filepath $NAME &
 done
+execute "no_such_file.cub" "no_such_file"
+execute 2>/dev/null
 wait
 killall cub3D 2>/dev/null
 echo -n
