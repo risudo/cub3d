00d12307fc1530b1655cb3d71b34e61b708bbe4f
rsudo <badbad7351@gmail.com>
2022-02-03 17:58:59 +0900

    fix still reacheable

--- src/game/destroy_and_exit.c
+++ src/game/destroy_and_exit.c
@@ -1,13 +1,15 @@
 #include "game.h"
 #include "mlx.h"
+#include "utils.h"
 #include <stdlib.h>
 
-static void	destroy_textures(t_game *game)
+static void	destroy_images(t_game *game)
 {
 	mlx_destroy_image(game->mlx, game->north_wall.img);
 	mlx_destroy_image(game->mlx, game->south_wall.img);
 	mlx_destroy_image(game->mlx, game->west_wall.img);
 	mlx_destroy_image(game->mlx, game->east_wall.img);
+	mlx_destroy_image(game->mlx, game->screen.img);
 }
 
 int	destroy_and_exit(void *arg)
@@ -16,7 +18,8 @@ int	destroy_and_exit(void *arg)
 
 	game = (t_game *)arg;
 	mlx_destroy_window(game->mlx, game->win);
-	destroy_textures(game);
+	destroy_images(game);
+	clear_string_array(game->map);
 	mlx_destroy_display(game->mlx);
 	free(game->mlx);
 	exit(0);
